b9799269b5263f7a231e28913fd2291d2c512a0d
Tien Minh Dang <dminh1@my.centennialcollege.ca>
2026-03-07 06:56:40 +0700

    Initial commit - COMP367 webapp

---
+++ src/main/java/edu/comp367/HelloServlet.java
@@ -0,0 +1,14 @@
+package edu.comp367;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("Welcome to COMP367");
+    }
+}
---
+++ target/comp367-webapp-0.0.1-SNAPSHOT/index.jsp
@@ -0,0 +1,5 @@
+<html>
+  <body>
+    Welcome to COMP367
+  </body>
+</html>
